02b4c1c54e4f1eb082c5a37b38c42f24d6bfeda6
Maxwell Carlson <bart4282819@gmail.com>
2021-04-11 18:52:21 -0700

    set padding for p1

--- Programming1/Programming1.py
+++ Programming1/Programming1.py
@@ -1,24 +1,36 @@
 import numpy as np
 from PIL import Image
 
-f1 = np.array(1.0/16 * [[1, 2, 1],[2, 4, 2],[1, 2, 1]])
-f2 = np.array(1.0/273 * [[1, 4, 7, 4, 1],[4, 16, 26, 16, 4],[7, 26, 41, 26, 7],
-                         [4, 16, 26, 16, 4],[1, 4, 7, 4, 1]])
-
+f1 = np.array([[1, 2, 1],[2, 4, 2],[1, 2, 1]], dtype='float')
+f1 *= 1.0/16
+f2 = np.array([[1, 4, 7, 4, 1],[4, 16, 26, 16, 4],[7, 26, 41, 26, 7],
+                         [4, 16, 26, 16, 4],[1, 4, 7, 4, 1]], dtype='float')
+f2 *= 1.0/273
 
 def applyFilter(filename, filter):
-    with Image.open(filename) as im:
-        ar = np.array(im)
+    with Image.open(filename) as image:
+        im = np.array(image)
 
-    sz = ar.shape
+    sz = im.shape
     fz = filter.shape[0]
 
-    out = np.array(ar.shape)
+    if fz == 3:
+        im = np.pad(im, 1)
+    else:
+        im = np.pad(im, 2)
 
-    for i in range(1, sz[0]-fz):
-        for j in range(1, sz[1]-fz):
-            out[i, j] = np.multiply(filter, ar[i:i+fz, j:j+fz])
+    out = np.zeros(im.shape)
 
+    zz = im[0:3, 0:3]
+
+    for i in range(0, sz[0]):
+        for j in range(0, sz[1]):
+            z = np.matmul(filter, im[i:i+fz, j:j+fz])
+            out[i][j] = np.sum(z)
+
+
+    img = Image.fromarray(out)
+    img.show()
     return out
 
-applyFilter('filter_img.jpg', f1)
+applyFilter('filter1_img.jpg', f1)
