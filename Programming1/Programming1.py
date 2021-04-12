0cda20bbaba13b25ba4bd58ef454f9844ff51edf
Maxwell Carlson <bart4282819@gmail.com>
2021-04-12 15:48:15 -0700

    p3 done

--- Programming1/Programming1.py
+++ Programming1/Programming1.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image, ImageOps
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 f1 = np.array([[1, 2, 1],[2, 4, 2],[1, 2, 1]], dtype='float')
 f1 *= 1.0/16
@@ -56,19 +57,90 @@ def sobel(filename):
 
 def keypoints(filename, idx):
     img = cv.imread(filename)
+    if idx == 2:
+        scale_percent = 40 # percent of original size
+        width = int(img.shape[1] * scale_percent / 100)
+        height = int(img.shape[0] * scale_percent / 100)
+        dim = (width, height)
+        # resize image
+        img = cv.resize(img, dim, interpolation = cv.INTER_AREA)
+  
+
     grey = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     sift = cv.SIFT_create()
-    kp = sift.detect(img)
+    kp, desc = sift.detectAndCompute(img, None)
 
-    img = cv.drawKeypoints(grey, kp, img)
+    outimg = img
+    img = cv.drawKeypoints(img, kp, outimg)
     cv.imwrite('sift_keypoints_{}.jpg'.format(idx),img)
     
-    return kp
+    return grey, kp, desc
 
 def imgmatch(f1, f2):
-    k1 = keypoints(f1, 1)
-    k2 = keypoints(f2, 2)
+    im1, k1, desc1 = keypoints(f1, 1)
+    im2, k2, desc2 = keypoints(f2, 2)
+
+    # Calculate L2 norms
+    pts = np.zeros((len(k1), 3))
+    for i, v in enumerate(desc1):
+        pts[i, 0]   = i
+        d1          = np.full(desc2.shape, v)
+        n           = np.linalg.norm(d1 - desc2, 2, axis=1)
+        minIdx      = np.argmin(n)
+        min         = n[minIdx]
+        pts[i, 1]   = minIdx
+        pts[i, 2]   = min
+    
+    # Sort array by the minimum l2 distance between SIFT vectors
+    pts = pts[pts[:,2].argsort()] 
+    
+    # Take lowest 10% of l2 distances
+    pts = pts[0:int(len(pts)/10)]
+
+    # Need to use one variable from these matches to build cv.DMatch
+    # Objects. L2 data is not used from these
+    matcher = cv.BFMatcher(cv.NORM_L2)
+    matchesCV = matcher.match(desc1, desc2)
+    matchesCV = sorted(matchesCV, key=lambda x: x.distance)
+
+    # Need to build cv.DMatch objects for simple plotting. 
+    matches = []
+    for i in range(len(pts)):
+        matches.append(cv.DMatch(_queryIdx=int(matchesCV[i].queryIdx), _imgIdx=int(pts[i, 0]), 
+                  _trainIdx=int(pts[i, 1]), _distance=pts[i, 2]))
+
+    img3 = cv.drawMatches(im1, k1, im2, k2, matches, im2, flags=2)
+    plt.imshow(img3)
+    plt.savefig('matches')
+    plt.show()
+
+
+    #l1, l2 = len(k1), len(k2)
+    #pts = np.zeros((len(k1), 3))
+    #
+    ## Turn SIFT vectors into numpy arrays
+    #kp1s = np.array([np.array(x.pt) for x in k1])
+    #kp2s = np.array([np.array(x.pt) for x in k2])
+    #
+    ## Calculate all l2 distances between k1 and k2 SIFT vectors
+    #for i, kp1 in enumerate(kp1s):
+    #    pts[i, 0]   = i
+    #    k1t         = np.full(kp2s.shape, kp1)
+    #    n           = np.linalg.norm(k1t - kp2s, 2, axis=1)
+    #    minIdx      = np.argmin(n)
+    #    min         = n[minIdx]
+    #    pts[i, 1]   = minIdx
+    #    pts[i, 2]   = min
+    #
+    ## Sort array by the minimum l2 distance between SIFT vectors
+    #pts = pts[pts[:,2].argsort()] 
+    #
+    ## Take lowest 10% of l2 distances
+    #pts = pts[0:int(len(pts)/10)]
+
+
+    
 
 
 #applyFilter('filter1_img.jpg', f1, 1)
